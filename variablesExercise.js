f126fc8c09cbd3a8f61ef53d8cfd9d525ad2eb5a
Travis M Whitworth <71463760+TravisMWhitworth@users.noreply.github.com>
2020-10-13 13:51:29 -0500

    Add files via upload

--- variablesExercise.js
+++ variablesExercise.js
@@ -1,22 +1,15 @@
 let myAge = 23;
-console.log(myAge);
 
-myAge = myAge - 5;
-console.log(myAge);
+myAge = 18;
 
 myAge += 6;
-console.log(myAge);
 
 myAge --;
-console.log(myAge);
 
  myAge = "None of your buisness";
-console.log("None of your buisness");
 
-let blank = 
-console.log(null);
+let blank; 
+blank = null;
 
-let notANumber = NaN;
-console.log("Not"[0]);
-console.log("Can't"[1]);
-console.log("Nada"[0]);
+let notANumber = 0 * Infinity;
+console.log(notANumber);
