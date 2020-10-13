a901dfbd86bf6748f8b92eb2849cc30d220b8832
Travis Whitworth <traviswhitworth@BCs-MacBook-Pro-5.local>
2020-10-13 13:09:22 -0500

    Completed VariablesExercise

---
+++ variablesExercise.js
@@ -0,0 +1,22 @@
+let myAge = 23;
+console.log(myAge);
+
+myAge = myAge - 5;
+console.log(myAge);
+
+myAge += 6;
+console.log(myAge);
+
+myAge --;
+console.log(myAge);
+
+ myAge = "None of your buisness";
+console.log("None of your buisness");
+
+let blank = 
+console.log(null);
+
+let notANumber = NaN;
+console.log("Not"[0]);
+console.log("Can't"[1]);
+console.log("Nada"[0]);
